5b7ffac16c40a0580ed01a2d3ae5c546fb922acc
温清茂 <qm-wen@mygzb.com>
2018-06-09 15:11:15 +0800

    fixed sh syntax error

--- docker/acs/start.sh
+++ docker/acs/start.sh
@@ -9,7 +9,7 @@ cd /app/genieacs && npm install
 echo "run npm run compile"
 cd /app/genieacs && npm run compile
 
-if [-f /app/genieacs/config/config-sample.json]
+if [ -f /app/genieacs/config/config-sample.json ]; then
   echo "mv config-sample.json to config.json"
   mv /app/genieacs/config/graphs-sample.json.erb /app/genieacs/config/config.json
 fi
--- docker/gui/start.sh
+++ docker/gui/start.sh
@@ -3,7 +3,7 @@ echo "mv samply.yml to .yml"
 for f in /app/genieacs-gui/config/*-sample.yml; do mv "$f" "${f/-sample.yml/.yml}"; done
 
 
-if [-f /app/genieacs-gui/config/graphs-sample.json.erb]
+if [ -f /app/genieacs-gui/config/graphs-sample.json.erb ]; then
   echo "mv graphs-sample.json.erb to graphs.json.erb"
   mv /app/genieacs-gui/config/graphs-sample.json.erb /app/genieacs-gui/config/graphs.json.erb
 fi
