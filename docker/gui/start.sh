277b66e5261ef3854d493dce998a63b857a47e0a
温清茂 <qm-wen@mygzb.com>
2018-06-09 15:40:57 +0800

    fixed gui start shell sytax error

--- docker/gui/start.sh
+++ docker/gui/start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "mv samply.yml to .yml"
-for f in /app/genieacs-gui/config/*-sample.yml; do mv "$f" "${f/-sample.yml/.yml}"; done
 
+for f in /app/genieacs-gui/config/*-sample.yml;
+do
+    mv "$f" "${f/-sample.yml/.yml}"
+done
 
 if [ -f /app/genieacs-gui/config/graphs-sample.json.erb ]; then
   echo "mv graphs-sample.json.erb to graphs.json.erb"
@@ -10,6 +13,9 @@ fi
 
 echo "set RAILS_ENV production bundle"
 cd /app/genieacs-gui && RAILS_ENV=production bundle && RAILS_ENV=production bundle exec rake assets:precompile
+
+echo "run bundle install"
+cd /app/genieacs-gui && bundle install
 
 echo "Start enieacs-gui"
 /app/genieacs-gui/bin/rails server
