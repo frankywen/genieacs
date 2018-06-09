6ecb5d185adad89bfb4943af8b520e862cb951f4
温清茂 <qm-wen@mygzb.com>
2018-06-09 11:25:17 +0800

    gui bundle

--- docker/gui/start.sh
+++ docker/gui/start.sh
@@ -8,5 +8,8 @@ if [-f /app/genieacs-gui/config/graphs-sample.json.erb]
   mv /app/genieacs-gui/config/graphs-sample.json.erb /app/genieacs-gui/config/graphs.json.erb
 fi
 
+echo "set RAILS_ENV production bundle"
+cd /app/genieacs-gui && RAILS_ENV=production bundle && RAILS_ENV=production bundle exec rake assets:precompile
+
 echo "Start enieacs-gui"
 /app/genieacs-gui/bin/rails server
