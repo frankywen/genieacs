f69e14a4da179fb6746c06d1cc8c5f01667bb6e1
温清茂 <qm-wen@mygzb.com>
2018-06-09 15:07:16 +0800

    acs start add config.json

--- docker/acs/start.sh
+++ docker/acs/start.sh
@@ -9,5 +9,10 @@ cd /app/genieacs && npm install
 echo "run npm run compile"
 cd /app/genieacs && npm run compile
 
+if [-f /app/genieacs/config/config-sample.json]
+  echo "mv config-sample.json to config.json"
+  mv /app/genieacs/config/graphs-sample.json.erb /app/genieacs/config/config.json
+fi
+
 echo "Start genieace-nbi"
 /app/genieacs/bin/genieacs-nbi
